1c3b73eb2d4468b21509de2f2530f40584558367
minaaduni <aminatshittu58@yahoo.com>
2023-02-22 10:32:01 -0800

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entery point
+ */
+void print_alphabet(void)
+{
+	char la;
+
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+
+	_putchar ('\n');
+}
