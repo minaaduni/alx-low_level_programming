e3c6ad12e710b9787199fd0744abf5fc74f29055
minaaduni <aminatshittu58@yahoo.com>
2023-02-23 03:05:44 -0800

    update

---
+++ 0x03.debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stidi.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("infinite loop avioded! \\o/\n");
+
+	return (0);
+}
+
