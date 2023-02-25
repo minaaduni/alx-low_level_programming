2d767ccc8c3145afceeba8c86e4abe83a14df68b
minaaduni <aminatshittu58@yahoo.com>
2023-02-25 11:43:22 -0800

    update

---
+++ 0x04-more_functions_nested_loops/3-prints_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: nothing on success
+ */
+void print_numbers(void)
+{
+	int start = 0, end = 9;
+
+	while (start <= end)
+	{
+		Putchar(start + '0');
+		star++;
+	}
+	_putchar('\n');
+}
