d29db422f05459bcdc78eb47c52ef7bf2763007c
minaaduni <aminatshittu58@yahoo.com>
2023-02-27 19:52:08 -0800

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,6 +5,7 @@
 
 /**
  * main - program that generates random valid
+ * passwords for the program 101-crackme
  * Return: Always 0 (success)
  */
 int main(void)
@@ -14,7 +15,7 @@ int main(void)
 
 	sum = 0;
 
-	srnd(time(NULL));
+	srand(time(NULL));
 
 	for (i = 0; i < 100; i++)
 	{
